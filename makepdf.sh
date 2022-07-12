6a0432db89e94380bd80af6b7a74bda85645f22d
Neel Smith <dnsmith.neel@gmail.com>
2022-07-12 10:31:55 -0400

    One-liner for PDF

---
+++ makepdf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+OUTFILE=pdf/hellenike.pdf
+COMPOSITE=composite.md
+SETTINGS=pdf/settings.yaml
+PANDOC=`which pandoc`
+JULIA=`which julia`
+echo $PANDOC plus $JULIA
+
+$JULIA composite.jl $SETTINGS $COMPOSITE
+$PANDOC $COMPOSITE --from markdown --template eisvogel --pdf-engine=tectonic --table-of-contents --number-sections  --top-level-division=chapter -o $OUTFILE
