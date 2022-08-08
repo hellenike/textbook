ec0f1f40cbc12906b55c3e45344ed37ec0be798b
Neel Smith <dnsmith.neel@gmail.com>
2022-08-08 12:23:53 -0400

    Update script for new syntax of params

--- makepdf.sh
+++ makepdf.sh
@@ -4,8 +4,9 @@ OUTFILE=pdf/hellenike.pdf
 COMPOSITE=composite.md
 SETTINGS=pdf/settings.yaml
 PANDOC=`which pandoc`
+SRC=docs
 JULIA=`which julia`
 echo Using $PANDOC and $JULIA
 
-$JULIA composite.jl $SETTINGS $COMPOSITE
+$JULIA composite.jl $SETTINGS $SRC $COMPOSITE
 $PANDOC $COMPOSITE --from markdown --template eisvogel --pdf-engine=tectonic --table-of-contents --number-sections  --top-level-division=chapter -o $OUTFILE
