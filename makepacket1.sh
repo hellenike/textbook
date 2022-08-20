1ee6024ee92327548ca930098d985f891deee568
Neel Smith <dnsmith.neel@gmail.com>
2022-08-20 07:34:29 -0400

    Update pdf build

--- makepacket1.sh
+++ makepacket1.sh
@@ -2,14 +2,14 @@
 
 OUTFILE=pdf/hellenike.pdf
 COMPOSITE=composite.md
-SETTINGS=pdf/settings.yaml
+SETTINGS=pdf/packet1.yaml
 PANDOC=`which pandoc`
 SRC=docs
 JULIA=`which julia`
 echo Using $PANDOC and $JULIA
 
 $JULIA composite.jl $SETTINGS $SRC $COMPOSITE
-$PANDOC $COMPOSITE --from markdown --template eisvogel --pdf-engine=tectonic --table-of-contents --number-sections  --top-level-division=chapter -o $OUTFILE
+$PANDOC $COMPOSITE --from markdown --template eisvogel --pdf-engine=tectonic --table-of-contents --number-sections  --top-level-division=chapter -o $OUTFILE -V classoption=oneside
 
 # -V 'mainfont:Lucida Grande'
 # -V classoption=oneside
