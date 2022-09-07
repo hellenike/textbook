769e611c399a846d455e2a8961cb97b66f88b907
Neel Smith <dnsmith.neel@gmail.com>
2022-09-07 08:31:48 -0400

    Improve sh script

--- makepdf.sh
+++ makepdf.sh
@@ -9,7 +9,7 @@ JULIA=`which julia`
 echo Using $PANDOC and $JULIA
 
 $JULIA composite.jl $SETTINGS $SRC $COMPOSITE
-$PANDOC $COMPOSITE --from markdown --template eisvogel --pdf-engine=tectonic --table-of-contents --number-sections  --top-level-division=chapter -o $OUTFILE
+$PANDOC $COMPOSITE --from markdown --template eisvogel --pdf-engine=tectonic -V paper:letter -V 'sansfont:Brill' -V 'mainfont:Brill' --table-of-contents --number-sections  --top-level-division=chapter -V classoption=oneside -o $OUTFILE
 
 # -V 'mainfont:Lucida Grande'
 # -V classoption=oneside
